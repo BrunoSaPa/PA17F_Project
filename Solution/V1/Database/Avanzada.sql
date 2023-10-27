b3a4f0fe302c9997cd27f64c754bc86d53c8c320
hisashl <awachip@gmail.com>
2023-10-26 19:29:23 -0600

    bd

--- Solution/V1/Database/Avanzada.sql
+++ Solution/V1/Database/Avanzada.sql
@@ -1,59 +1,24 @@
 --
--- File generated with SQLiteStudio v3.4.4 on mié. oct. 25 19:10:34 2023
+-- File generated with SQLiteStudio v3.4.4 on jue. oct. 26 18:55:54 2023
 --
--- Text encoding used: UTF-8
+-- Text encoding used: System
 --
 PRAGMA foreign_keys = off;
 BEGIN TRANSACTION;
 
 -- Table: almacenistas
-DROP TABLE IF EXISTS almacenistas;
+CREATE TABLE IF NOT EXISTS almacenistas (id INTEGER PRIMARY KEY, id_usuario INTEGER REFERENCES usuarios (id), FOREIGN KEY (id_usuario) REFERENCES usuarios (id));
 
-CREATE TABLE IF NOT EXISTS almacenistas (
-    id               INTEGER  PRIMARY KEY,
-    id_usuario       INTEGER,
-    fch_creacion     DATETIME DEFAULT (datetime('now') ),
-    fch_modificacion DATETIME,
-    fch_eliminacion  DATETIME,
-    FOREIGN KEY (
-        id_usuario
-    )
-    REFERENCES usuarios (id) 
-);
-
+-- Table: contiene
+CREATE TABLE IF NOT EXISTS contiene (id INTEGER PRIMARY KEY, id_prm_prestamo INTEGER, id_equipo INTEGER, FOREIGN KEY (id_prm_prestamo) REFERENCES prm_prestamos (id), FOREIGN KEY (id_equipo) REFERENCES equipos (id));
 
 -- Table: coordinadores
-DROP TABLE IF EXISTS coordinadores;
-
-CREATE TABLE IF NOT EXISTS coordinadores (
-    id               INTEGER  PRIMARY KEY,
-    id_usuario       INTEGER,
-    nomina           INTEGER,
-    fch_creacion     DATETIME DEFAULT (datetime('now') ),
-    fch_modificacion DATETIME,
-    fch_eliminacion  DATETIME,
-    FOREIGN KEY (
-        id_usuario
-    )
-    REFERENCES usuarios (id) 
-);
-
+CREATE TABLE IF NOT EXISTS coordinadores (id INTEGER PRIMARY KEY, id_usuario INTEGER REFERENCES usuarios (id), FOREIGN KEY (id_usuario) REFERENCES usuarios (id));
 
 -- Table: divisiones
-DROP TABLE IF EXISTS divisiones;
-
-CREATE TABLE IF NOT EXISTS divisiones (
-    id               INTEGER PRIMARY KEY AUTOINCREMENT,
-    descripcion      TEXT,
-    fch_creacion     TEXT    DEFAULT (datetime('now') ),
-    fch_modificacion TEXT    DEFAULT '0000-00-00 00:00:00',
-    fch_eliminacion  TEXT    DEFAULT '0000-00-00 00:00:00'
-);
-
+CREATE TABLE IF NOT EXISTS divisiones (id INTEGER PRIMARY KEY, nombre VARCHAR (50));
 
 -- Table: ensena
-DROP TABLE IF EXISTS ensena;
-
 CREATE TABLE IF NOT EXISTS ensena (
     id          INTEGER PRIMARY KEY,
     id_grupo    INTEGER,
@@ -68,63 +33,19 @@ CREATE TABLE IF NOT EXISTS ensena (
     REFERENCES profesores (id) 
 );
 
-
 -- Table: equipos
-DROP TABLE IF EXISTS equipos;
-
-CREATE TABLE IF NOT EXISTS equipos (
-    id             INTEGER PRIMARY KEY,
-    cnt_disponible INTEGER,
-    nombre         TEXT,
-    descripcion    TEXT
-);
-
-
--- Table: est_usuarios
-DROP TABLE IF EXISTS est_usuarios;
-
-CREATE TABLE IF NOT EXISTS est_usuarios (
-    id          INTEGER PRIMARY KEY AUTOINCREMENT,
-    descripcion TEXT
-);
-
-INSERT INTO est_usuarios (id, descripcion) VALUES (1, 'Activo');
-INSERT INTO est_usuarios (id, descripcion) VALUES (2, 'Inactivo');
+CREATE TABLE IF NOT EXISTS equipos (id INTEGER PRIMARY KEY, cnt_disponible INTEGER, nombre VARCHAR (70), descripcion TEXT);
 
 -- Table: estudiantes
-DROP TABLE IF EXISTS estudiantes;
-
-CREATE TABLE IF NOT EXISTS estudiantes (
-    id               INTEGER  PRIMARY KEY,
-    id_usuario       INTEGER,
-    id_grupo         INTEGER,
-    registro         INTEGER,
-    fch_creacion     DATETIME DEFAULT (datetime('now') ),
-    fch_modificacion DATETIME,
-    fch_eliminacion  DATETIME,
-    FOREIGN KEY (
-        id_usuario
-    )
-    REFERENCES usuarios (id),
-    FOREIGN KEY (
-        id_grupo
-    )
-    REFERENCES grupos (id) 
-);
-
+CREATE TABLE IF NOT EXISTS estudiantes (id INTEGER PRIMARY KEY, id_usuario INTEGER, id_grupo INTEGER, FOREIGN KEY (id_usuario) REFERENCES usuarios (id), FOREIGN KEY (id_grupo) REFERENCES grupos (id));
 
 -- Table: grupos
-DROP TABLE IF EXISTS grupos;
-
 CREATE TABLE IF NOT EXISTS grupos (
     id     INTEGER PRIMARY KEY,
     nombre TEXT
 );
 
-
 -- Table: imparte
-DROP TABLE IF EXISTS imparte;
-
 CREATE TABLE IF NOT EXISTS imparte (
     id          INTEGER PRIMARY KEY,
     id_profesor INTEGER,
@@ -139,175 +60,55 @@ CREATE TABLE IF NOT EXISTS imparte (
     REFERENCES materias (id) 
 );
 
+-- Table: maneja
+CREATE TABLE IF NOT EXISTS maneja (id INTEGER PRIMARY KEY, id_tps_mantenimiento REFERENCES tps_mantenimiento (id), id_mantenimiento INTEGER REFERENCES mantenimiento (id));
 
 -- Table: mantenimiento
-DROP TABLE IF EXISTS mantenimiento;
-
-CREATE TABLE IF NOT EXISTS mantenimiento (
-    id                   INTEGER  PRIMARY KEY,
-    id_tpo_mantenimiento INTEGER,
-    id_equipo            INTEGER,
-    descripciones        TEXT,
-    refaccion            TEXT,
-    fch_inicio           DATETIME,
-    fch_fin              DATETIME,
-    fch_creacion         DATETIME DEFAULT (datetime('now') ),
-    fch_modificacion     DATETIME,
-    fch_eliminacion      DATETIME
-);
-
+CREATE TABLE IF NOT EXISTS mantenimiento (id INTEGER PRIMARY KEY, id_tpo_mantenimiento INTEGER, id_equipo INTEGER REFERENCES equipos (id), descripcion TEXT, refaccion TEXT, fch_inicio DATETIME, fch_fin DATETIME);
 
 -- Table: materias
-DROP TABLE IF EXISTS materias;
-
-CREATE TABLE IF NOT EXISTS materias (
-    id               INTEGER PRIMARY KEY AUTOINCREMENT,
-    id_division      INTEGER,
-    descripcion      TEXT,
-    fch_creacion     TEXT    DEFAULT (datetime('now') ),
-    fch_modificacion TEXT    DEFAULT '0000-00-00 00:00:00',
-    fch_eliminacion  TEXT    DEFAULT '0000-00-00 00:00:00',
-    FOREIGN KEY (
-        id_division
-    )
-    REFERENCES divisiones (id) 
-);
-
+CREATE TABLE IF NOT EXISTS materias (id INTEGER PRIMARY KEY AUTOINCREMENT, id_division INTEGER, nombre VARCHAR (60), FOREIGN KEY (id_division) REFERENCES divisiones (id));
 
 -- Table: prm_prestamos
-DROP TABLE IF EXISTS prm_prestamos;
-
 CREATE TABLE IF NOT EXISTS prm_prestamos (
     id                  INTEGER  PRIMARY KEY AUTOINCREMENT,
+    id_salones          INTEGER  REFERENCES salones (id),
     id_tpo_prm_prestamo INTEGER,
-    id_usuario          INTEGER,
     fch_inicio          DATETIME DEFAULT (datetime('now') ),
     fch_fin             DATETIME,
-    fch_creacion        DATETIME DEFAULT (datetime('now') ),
     fch_modificacion    DATETIME,
     fch_eliminacion     DATETIME,
+    fch_creacion        DATETIME,
     FOREIGN KEY (
         id_tpo_prm_prestamo
     )
-    REFERENCES tps_prm_prestamos (id),
-    FOREIGN KEY (
-        id_usuario
-    )
-    REFERENCES usuarios (id) 
-);
-
+    REFERENCES tps_prm_prestamos (id));
 
 -- Table: profesores
-DROP TABLE IF EXISTS profesores;
+CREATE TABLE IF NOT EXISTS profesores (id INTEGER PRIMARY KEY, id_usuario INTEGER, FOREIGN KEY (id_usuario) REFERENCES usuarios (id));
 
-CREATE TABLE IF NOT EXISTS profesores (
-    id               INTEGER  PRIMARY KEY,
-    id_usuario       INTEGER,
-    nomina           INTEGER,
-    fch_creacion     DATETIME DEFAULT (datetime('now') ),
-    fch_modificacion DATETIME,
-    fch_eliminacion  DATETIME,
-    FOREIGN KEY (
-        id_usuario
-    )
-    REFERENCES usuarios (id) 
-);
+-- Table: puede
+CREATE TABLE IF NOT EXISTS puede (id INTEGER PRIMARY KEY, id_usuario INTEGER REFERENCES usuarios (id), id_prm_prestamo INTEGER REFERENCES prm_prestamos (id));
 
-
--- Table: rlc_prs_equipo
-DROP TABLE IF EXISTS rlc_prs_equipo;
-
-CREATE TABLE IF NOT EXISTS rlc_prs_equipo (
-    id              INTEGER PRIMARY KEY,
-    id_prm_prestamo INTEGER,
-    id_equipo       INTEGER,
-    FOREIGN KEY (
-        id_prm_prestamo
-    )
-    REFERENCES prm_prestamos (id),
-    FOREIGN KEY (
-        id_equipo
-    )
-    REFERENCES equipos (id) 
-);
-
+-- Table: recibe
+CREATE TABLE IF NOT EXISTS recibe (id INTEGER PRIMARY KEY, id_mantenimiento INTEGER REFERENCES tps_mantenimiento (id), id_equipo INTEGER REFERENCES equipos (id));
 
 -- Table: salones
-DROP TABLE IF EXISTS salones;
-
-CREATE TABLE IF NOT EXISTS salones (
-    id          INTEGER PRIMARY KEY,
-    id_division INTEGER,
-    nmr_salon   INTEGER,
-    FOREIGN KEY (
-        id_division
-    )
-    REFERENCES divisiones (id) 
-);
-
+CREATE TABLE IF NOT EXISTS salones (id INTEGER PRIMARY KEY, nombre VARCHAR (40));
 
 -- Table: tps_mantenimiento
-DROP TABLE IF EXISTS tps_mantenimiento;
-
-CREATE TABLE IF NOT EXISTS tps_mantenimiento (
-    id          INTEGER PRIMARY KEY AUTOINCREMENT,
-    descripcion TEXT
-);
-
+CREATE TABLE IF NOT EXISTS tps_mantenimiento (id INTEGER PRIMARY KEY AUTOINCREMENT, descripcion VARCHAR (40));
 INSERT INTO tps_mantenimiento (id, descripcion) VALUES (1, 'Preventido');
 INSERT INTO tps_mantenimiento (id, descripcion) VALUES (2, 'Correctivo');
 INSERT INTO tps_mantenimiento (id, descripcion) VALUES (3, 'Predictivo');
 
 -- Table: tps_prm_prestamos
-DROP TABLE IF EXISTS tps_prm_prestamos;
-
-CREATE TABLE IF NOT EXISTS tps_prm_prestamos (
-    id          INTEGER PRIMARY KEY AUTOINCREMENT,
-    descripcion TEXT
-);
-
+CREATE TABLE IF NOT EXISTS tps_prm_prestamos (id INTEGER PRIMARY KEY AUTOINCREMENT, descripcion VARCHAR (40));
 INSERT INTO tps_prm_prestamos (id, descripcion) VALUES (1, 'Generado por un estudiante');
 INSERT INTO tps_prm_prestamos (id, descripcion) VALUES (2, 'Generado por un profesor');
 
--- Table: tps_usuarios
-DROP TABLE IF EXISTS tps_usuarios;
-
-CREATE TABLE IF NOT EXISTS tps_usuarios (
-    id          INTEGER PRIMARY KEY AUTOINCREMENT,
-    descripcion TEXT
-);
-
-INSERT INTO tps_usuarios (id, descripcion) VALUES (1, 'Almacenista');
-INSERT INTO tps_usuarios (id, descripcion) VALUES (2, 'Estudiante');
-INSERT INTO tps_usuarios (id, descripcion) VALUES (3, 'Profesor');
-INSERT INTO tps_usuarios (id, descripcion) VALUES (4, 'Director');
-
 -- Table: usuarios
-DROP TABLE IF EXISTS usuarios;
-
-CREATE TABLE IF NOT EXISTS usuarios (
-    id               INTEGER  PRIMARY KEY AUTOINCREMENT
-                              NOT NULL,
-    id_tpo_usuario   INTEGER  NOT NULL,
-    id_est_usuario   INTEGER  NOT NULL,
-    contrasena       BLOB,
-    nombre           TEXT     NOT NULL
-                              DEFAULT '',
-    apl_materno      TEXT,
-    apl_paterno      TEXT,
-    fch_creacion     DATETIME DEFAULT (datetime('now') ),
-    fch_modificacion DATETIME,
-    fch_eliminacion  DATETIME,
-    FOREIGN KEY (
-        id_tpo_usuario
-    )
-    REFERENCES tps_usuario (id),
-    FOREIGN KEY (
-        id_est_usuario
-    )
-    REFERENCES est_usuario (id) 
-);
-
+CREATE TABLE IF NOT EXISTS usuarios (id INTEGER PRIMARY KEY, nombre VARCHAR (40), apellido_P VARCHAR (50), apellido_M VARCHAR (50), contrasena VARCHAR (15) NOT NULL);
 
 COMMIT TRANSACTION;
 PRAGMA foreign_keys = on;
